c23275bb2450f1b41f7cd77895c484cd46203da9
frodosamoa <andrewrkowalczyk@gmail.com>
2013-11-11 20:38:25 -0800

    added routes for categories and graphs, connecting to the days collection in the hbt database

--- hbt/hbt.py
+++ hbt/hbt.py
@@ -1,27 +1,27 @@
 import json
-import bottle
-import pymongo
 import Habit
+import Day
 import cgi
 import re
 
-from bottle import route, get, post, put, debug, run, request, redirect, template
+from bottle import Bottle, route, get, post, put, debug, run, request, redirect, template, static_file 
+# get_url
 from pymongo import MongoClient
 
 @route('/')
 def get_habits():
     l = habits.get_habits(10)
-    return template('habits_template', dict(myhabits=l))
+    # return template('habits_template', dict(myhabits=l), get_url=get_url)
+    return template('habits', dict(myhabits=l))
 
 
 @route('/login')
 def login():
     return template('login')
 
-@get('/newhabit')
+@route('/newhabit')
 def newhabit():
-
-    return template('newhabit_template', dict(name='', times = '', occurence='', reminders=''))
+    return template('new_habit', dict(name='', times = '', occurence='', reminders='', categories=''))
 
 @post('/newhabit')
 def post_new_habit():
@@ -34,6 +34,14 @@ def post_new_habit():
 
     redirect('/')
 
+@route('/categories')
+def get_categories():
+    return template('categories');
+
+@route('/graphs')
+def get_graphs():
+    return template('graphs');
+
 @put('/edithabit')
 def edit_habit():
     name = request.forms.get('name')
@@ -42,7 +50,7 @@ def edit_habit():
     reminders = request.forms.get('reminders')
     categories = request.forms.get('categories')
 
-    habits.insert_habit(name, times, occurence, reminders, categories)
+    habits.update(name, times, occurence, reminders, categories)
 
     redirect('/')
 
@@ -51,7 +59,7 @@ client = MongoClient('localhost', 27017)
 database = client.hbt
 
 habits = Habit.Habit(database)
-# checks = Checks.Check(database)
+days = Day.Day(database)
 
 debug(True)
 run(host='localhost', port=8080) 
