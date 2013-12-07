eec98690c9b0af2d00b8a89702069e36abbef5f4
frodosamoa <andrewrkowalczyk@gmail.com>
2013-12-06 21:02:13 -0800

    using check_logged_in for all necessary routes

--- hbt/hbt.py
+++ hbt/hbt.py
@@ -2,32 +2,50 @@ import json
 import Habit
 import Day
 import Session
+import User
+import cgi
+import re
 
-from bottle import route, get, post, put, debug, run, request, redirect, template, static_file, error 
-# get_url
+from bottle import route, get, post, put, debug, run, request, redirect, template, static_file, error, response
 from pymongo import MongoClient
 
 
-
-@route('/signin')
-def signin():
-    return template('signin')
+####
+# ROUTES
+####
 
 @route('/signup')
-def signup():
-    return template('signup')
+def get_signup():
+    return template('signup', dict(username='', password='', password_error='', username_error='', verify_error =''))
 
-
+@route('/signin')
+def get_signin():
+    return template('signin', dict(username='', password='', login_error=''))
 
 @route('/')
 def get_habits():
-    l = habits.get_habits(10)
-    return template('habits', dict(myhabits=l))
-
+    username = check_logged_in()
+    l = habits.get_habits(username)
+    return template('habits', dict(username=username, myhabits=l))
 
 @route('/newhabit')
 def newhabit():
-    return template('new_habit', dict(name='', times = '', occurence='', reminders='', categories=''))
+    username = check_logged_in()
+    return template('new_habit', dict(username=username, name='', times = '', occurence='', reminders='', categories=''))
+
+@route('/categories')
+def get_categories():
+    username = check_logged_in()
+    return template('categories', dict(username=username));
+
+@route('/graphs')
+def get_graphs():
+    username = check_logged_in()
+    return template('graphs', dict(username=username));
+
+####
+# POSTS
+####
 
 @post('/newhabit')
 def post_new_habit():
@@ -36,23 +54,13 @@ def post_new_habit():
     occurence = request.forms.get('occurence')
     reminders = request.forms.get('reminders')
     categories = request.forms.get('categories').split(',')
-    habits.insert_habit(name, times, occurence, reminders, categories)
+    habits.insert_habit(username, name, times, occurence, reminders, categories)
 
     redirect('/')
 
-
-
-
-@route('/categories')
-def get_categories():
-    return template('categories');
-
-@route('/graphs')
-def get_graphs():
-    return template('graphs');
-
-
-
+####
+# PUTS
+####
 
 @put('/edithabit')
 def edit_habit():
@@ -82,11 +90,22 @@ def server_static(filename):
 def error404(error):
     return template('404')
 
+# helper function to see if we are logged in
+def check_logged_in():
+    cookie = request.get_cookie("session")
+    username = sessions.get_username(cookie)
+    if username is None:
+        redirect("/signup")
+    else:
+        return username
+
 
 client = MongoClient('localhost', 27017)
 database = client.hbt
+
 habits = Habit.Habit(database)
 sessions = Session.Session(database)
+users = User.User(database)
 
 debug(True)
 run(host='localhost', port=8080) 
