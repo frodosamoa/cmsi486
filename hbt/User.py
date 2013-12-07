1bcd3d431434e478a2584966b7741b486a12d8a8
frodosamoa <andrewrkowalczyk@gmail.com>
2013-12-06 20:07:22 -0800

    adding user.py file

---
+++ hbt/User.py
@@ -0,0 +1,20 @@
+import pymongo
+
+class User:
+
+    def __init__(self, connection):
+        self.connection = pymongo.MongoClient('localhost', 27017)
+        self.users = self.connection.hbt.users
+        self.SECRET = 'wow such secret'
+
+    def add_user(self, username, password):
+
+        user = {'_id': username, 'password': password}
+
+        try:
+            self.users.insert(user, safe=True)
+        except pymongo.errors.DuplicateKeyError as e:
+            print "that username is already taken"
+            return False
+
+        return True
