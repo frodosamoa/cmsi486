ef0662eab0bf6bf5938e7148a4724863e1180fad
frodosamoa <andrewrkowalczyk@gmail.com>
2013-12-06 20:43:41 -0800

    let's end sessions as well

--- hbt/Session.py
+++ hbt/Session.py
@@ -9,6 +9,9 @@ class Session:
         self.connection = pymongo.MongoClient('localhost', 27017)
         self.sessions = self.connection.hbt.sessions
 
+    def random_string(self, num_chars=32, chars=string.ascii_letters):
+        return ''.join(random.choice(chars) for x in range(num_chars))
+
     def begin_session(self, username):
 
         session_id = self.random_string()
@@ -23,5 +26,11 @@ class Session:
 
         return str(session['_id'])
 
-    def random_string(self, num_chars=32, chars=string.ascii_letters):
-        return ''.join(random.choice(chars) for x in range(num_chars))
+    def end_session(self, session_id):
+
+        if session_id is None:
+            return
+
+        self.sessions.remove({'_id': session_id})
+
+        return
