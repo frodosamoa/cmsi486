2e0012910dac48d3c476f7b69b4f69d97becb158
frodosamoa <andrewrkowalczyk@gmail.com>
2013-12-10 22:53:52 -0800

    removed centering spacing for the table

--- hbt/static/js/habits.js
+++ hbt/static/js/habits.js
@@ -3,8 +3,6 @@ $(function() {
 	$('#habit-name-row-table > tbody > tr, #habit-actions').affix({
 	    offset: { top: $('#habit-history').offset().top - $('.navbar').height()}
 	});
-
-	$('#habit-name-row-table, #habit-table').css('margin-left', -$('#habit-name-row-table').width() / 2);
 	
 	$('#habit-table td').on('click', function() {
 		if ($(this).attr('disabled') !== "disabled") {
