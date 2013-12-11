34cc46e444803517850f9cad93e953e7a2468bc1
frodosamoa <andrewrkowalczyk@gmail.com>
2013-12-10 20:10:37 -0800

    added completed intervals into the habit model, created get_habit funciton, and created refresh habits function for when a user logs in

--- hbt/Habit.py
+++ hbt/Habit.py
@@ -10,20 +10,21 @@ class Habit:
         self.habits = self.connection.hbt.habits
 
 
-    def insert_habit(self, username, name, times, occurence, reminders, categories):
-        print 'inserting habit', username, name, times, occurence, reminders, categories
+    def insert_habit(self, username, name, interval, occurence, reminders, categories):
+        print 'inserting habit', username, name, interval, occurence, reminders, categories
 
         today = datetime.datetime.now().date()
 
-        habit = {'username' : username,
-                 'name': name,
-                 'interval' : {
-                        'times' : times,
-                        'occurence' : occurence
-                    },
-                 'reminders' : reminders,
-                 'categories' : categories,
-                 'dateCreated' : str(today)
+        habit = { 'username'           : username,
+                  '_id'                : name,
+                  'interval'           : interval,
+                  'occurence'          : occurence,
+                  'reminders'          : reminders,
+                  'categories'         : categories,
+                  'dateCreated'        : str(today), 
+                  'completedIntervals' : {
+                        '0': False
+                  }
                 }
         try:
             self.habits.insert(habit, safe=True)
@@ -40,19 +41,44 @@ class Habit:
         l = []
 
         for habit in cursor:
-            l.append({'name': habit['name'],
-                      'interval' : {
-                          'times' : habit['interval']['times'],
-                          'occurence' : habit['interval']['occurence']
-                        },
+            l.append({'name' : habit['_id'],
+                      'interval' : habit['interval'],
+                      'occurence' : habit['occurence'],
                       'reminders' : habit['reminders'],
                       'categories' : habit['categories'],
-                      'dateCreated' : habit['dateCreated']})
+                      'dateCreated' : habit['dateCreated'],
+                      'completedIntervals' : habit['completedIntervals']})
 
         return l
 
-    def update_habit(self):
-        return 0
+    def get_habit(self, name):
+        cursor = self.habits.find({'_id': name})
+
+        habit = {'name': cursor[0]['_id'],
+                  'interval' : cursor[0]['interval'],
+                  'occurence' : cursor[0]['occurence'],
+                  'reminders' : cursor[0]['reminders'],
+                  'categories' : cursor[0]['categories'],
+                  'dateCreated' : cursor[0]['dateCreated'],
+                  'completedIntervals' : habit['completedIntervals']}
+
+        return habit
+
+    def update_habit_intervals(self, habit):
+        self.habits.update({'_id' : habit['name']}, {'$set': {'completedIntervals' : habit['completedIntervals']} }, upsert=False)
+
+    def refresh_habits(self, username):
+        habits = self.get_user_habits(username)
+        today = datetime.datetime.now().date()
+        
+        for habit in habits:
+            time_delta = today - datetime.datetime.strptime(habit['dateCreated'], "%Y-%m-%d").date()
+            print habit['completedIntervals']
+            print time_delta.days
+            for day in range(time_delta.days):
+                if str(day) not in habit['completedIntervals']:
+                    habit['completedIntervals'][str(day)] = False
+            self.update_habit_intervals(habit)
 
     def get_oldest_habit_date(self, username):
         cursor = self.habits.find({'username' : username}).sort('dateCreated', pymongo.ASCENDING).limit(1)
@@ -66,11 +92,9 @@ class Habit:
         l = []
 
         for habit in cursor:
-            l.append({'name': habit['name'],
-                      'interval' : {
-                          'times' : habit['interval']['times'],
-                          'occurence' : habit['interval']['occurence']
-                        },
+            l.append({'name': habit['_id'],
+                      'interval' : habit['interval'],
+                      'occurence' : habit['occurence'],
                       'reminders' : habit['reminders'],
                       'categories' : habit['categories']})
 
