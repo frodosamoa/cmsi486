5032fc03b5b6c83767d8b5a98459473ef44b8c93
frodosamoa <andrewrkowalczyk@gmail.com>
2013-12-10 23:43:21 -0800

    adding glyphs to see if a habit was completed or not

--- hbt/static/js/habits.js
+++ hbt/static/js/habits.js
@@ -7,6 +7,8 @@ $(function() {
 	$('#habit-table td').on('click', function() {
 		if ($(this).attr('disabled') !== "disabled") {
 			$(this).toggleClass('success danger');
+			$(this).html($(this).hasClass("success") ? "<span class=\"glyphicon glyphicon-ok complete\"></span>"
+													 : "<span class=\"glyphicon glyphicon-remove todo\"></span>");
 			var habitInstance = $(this).attr('name');
 			var value = $("#" + habitInstance).attr('value');
 			$("#" + habitInstance).attr('value', value === "true" ? "false" : "true");
